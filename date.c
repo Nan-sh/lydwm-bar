5d1059ee5d231e04208e69696e7c29a5c21b0f12
Nan-sh <49915702+Nan-sh@users.noreply.github.com>
2022-02-28 15:45:33 +0800

    Update date.c

--- date.c
+++ date.c
@@ -5,6 +5,13 @@
 void getDate(char *str)
 {
     char *arr[] = {"日", "一", "二", "三", "四", "五", "六"};
+    char hour[4];
+    char min[4];
+    char sec[4];
+
+    memset(hour, 0, 4);
+    memset(min, 0, 4);
+    memset(sec, 0, 4);
 
     time_t t;
     struct tm * lt;
@@ -20,5 +27,26 @@ void getDate(char *str)
     info.tm_mday = lt->tm_mday;
     mktime(&info);
 
-    sprintf(str, " %s %d/%d/%d %d:%d:%d", arr[info.tm_wday], lt->tm_year+1900, lt->tm_mon + 1, lt->tm_mday, lt->tm_hour, lt->tm_min, lt->tm_sec);//输出结果
+    if(lt->tm_hour < 10)
+    {
+	sprintf(hour, "0%d", lt->tm_hour);
+    }
+    else
+	sprintf(hour, "%d", lt->tm_hour);    
+
+    if(lt->tm_min < 10)
+    {
+	sprintf(min, "0%d", lt->tm_min);
+    }
+    else
+	sprintf(min, "%d", lt->tm_min);
+
+    if(lt->tm_sec < 10)
+    {
+	sprintf(sec, "0%d", lt->tm_sec);
+    }
+    else
+	sprintf(sec, "%d", lt->tm_sec);
+
+    sprintf(str, " %s %d/%d/%d %s:%s:%s", arr[info.tm_wday], lt->tm_year+1900, lt->tm_mon + 1, lt->tm_mday, hour, min, sec);//输出结果
 }
